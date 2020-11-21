ffc4ebe528d082b111410a62004fd0f67aa7510d
John Do <jdo697@gmail.com>
2020-11-21 12:40:58 -0800

    saved

--- components/app.js
+++ components/app.js
@@ -12,11 +12,12 @@ class App {
     var newAverage = total / grades.length;
     this.pageHeader.updateAverage(newAverage)
   }
-  constructor(gradeTable, pageHeader) {
+  constructor(gradeTable, pageHeader, gradeForm) {
     this.handleGetGradesError = this.handleGetGradesError.bind(this);
     this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this)
     this.gradeTable = gradeTable
     this.pageHeader = pageHeader
+    this.gradeForm = gradeForm
   }
   getGrades() {
     $.ajax({
