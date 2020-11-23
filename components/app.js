7340db1b854e2f4c4eb4eb2633cc9e2d5a8c9216
John Do <jdo697@gmail.com>
2020-11-23 09:20:08 -0800

    user can add a grade

--- components/app.js
+++ components/app.js
@@ -4,6 +4,7 @@ class App {
   }
   handleGetGradesSuccess(grades) {
     this.gradeTable.updateGrades(grades)
+    this.getGrades()
 
     var total = 0
     for (var i = 0; i < grades.length; i++) {
@@ -12,9 +13,27 @@ class App {
     var newAverage = total / grades.length;
     this.pageHeader.updateAverage(newAverage)
   }
+  createGrade(name, course, grade) {
+    console.log(name, course, grade)
+    $.ajax({
+      headers: {
+        "X-Access-Token": "QL4ulI2r"
+      },
+      url: 'https://sgt.lfzprototypes.com/api/grades',
+      method: 'POST',
+      data: {
+        name: 'Jane Doe',
+        course: 'Math 101',
+        grade: 100
+      },
+      error: this.handleGetGradesError,
+      success: this.handleGetGradesSuccess
+    })
+  }
   constructor(gradeTable, pageHeader, gradeForm) {
     this.handleGetGradesError = this.handleGetGradesError.bind(this);
     this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this)
+    this.createGrade = this.createGrade.bind(this)
     this.gradeTable = gradeTable
     this.pageHeader = pageHeader
     this.gradeForm = gradeForm
@@ -32,5 +51,6 @@ class App {
   }
   start() {
     this.getGrades()
+    this.gradeForm.onSubmit(this.createGrade)
   }
 }
--- components/grade-form.js
+++ components/grade-form.js
@@ -2,12 +2,22 @@ class GradeForm {
   constructor(formElement) {
     this.formElement = formElement
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.formElement.addEventListener('submit', this.handleSubmit)
   }
   onSubmit(createGrade) {
     this.createGrade = createGrade
   }
   handleSubmit(event) {
     event.preventDefault()
-    console.log('handleSubmit called')
+
+    var formData = new FormData(event.target)
+    var formName = formData.get('name');
+    var formCourse = formData.get('course');
+    var formGrade = formData.get('grade');
+
+    this.createGrade(formName, formCourse, formGrade)
+
+    event.target.reset()
+
   }
 }
