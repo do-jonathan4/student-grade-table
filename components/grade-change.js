8008bafcd012d79322d8c83649e5fdf30d36f8ef
John Do <jdo697@gmail.com>
2021-01-25 04:54:18 -0800

    saved

--- components/grade-change.js
+++ components/grade-change.js
@@ -3,9 +3,24 @@ class GradeChange {
     this.updateElement = updateElement
     // this.handleSubmit = this.handleSubmit.bind(this)
     // this.updateElement.addEventListener('submit', this.handleSubmit)
+    this.updateElement.addEventListener('reset', this.nevermind)
   }
   copyUser(data) {
     console.log(data)
+    formUpdate.parentNode.classList.remove('d-none')
+    // this.updateElement.parentNode.classList.remove('d-none')
+    form.parentNode.classList.add('d-none')
+
+    var changeName = document.getElementById('changeName')
+    var changeCourse = document.getElementById('changeCourse')
+    var changeGrade = document.getElementById('changeGrade')
+
+    changeName.value = data.name
+    changeCourse.value = data.course
+    changeGrade.value = data.grade
+  }
+  nevermind(event) {
+    console.log('hello')
   }
   // onSubmit(changeGrade) {
   //   this.changeGrade = changeGrade
@@ -21,5 +36,7 @@ class GradeChange {
   //   this.changeGrade(formName, formCourse, formGrade)
 
   //   event.target.reset()
+  // form.parentNode.classList.remove('d-none')
+  // formUpdate.parentNode.classList.add('d-none')
   // }
 }
